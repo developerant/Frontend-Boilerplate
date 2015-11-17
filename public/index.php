db4dafaee39ab2494be25ce23817578652a8803d
Ross Bennett <rossdis@gmail.com>
2015-11-17 12:56:08 +0000

    chore(*): clean SVG inject

--- public/index.php
+++ public/index.php
@@ -9,8 +9,8 @@
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Favicons -->
 		<link rel="apple-touch-icon" href="apple-touch-icon.png">
-		<!-- Favicons -->
 
 		<!-- Humans.txt -->
 		<link type="text/plain" rel="author" href="/humans.txt" />
@@ -23,13 +23,11 @@
 
 		<div style="display:none">
 			<!-- inject:svg -->
-			<svg style="position:absolute"><symbol class="icon" viewBox="37.5 212.8 520.3 416.2" id="twitter"><title>Twitter Icon</title><desc>Link to Twitter page</desc><path d="M543.4 220.5c-20.6 12.1-43.5 20.8-67.8 25.5-19.4-20.4-47.2-33.2-77.9-33.2-58.9 0-106.7 47.1-106.7 105.1 0 8.2.9 16.2 2.7 24-88.7-4.4-167.4-46.2-220-109.8-9.2 15.5-14.5 33.5-14.5 52.8 0 36.4 18.8 68.6 47.5 87.4-17.5-.5-34-5.3-48.4-13.2v1.3c0 50.9 36.8 93.4 85.6 103-9 2.4-18.4 3.7-28.1 3.7-6.8 0-13.6-.6-20.1-1.8 13.6 41.7 53 72.1 99.7 73-36.6 28.2-82.5 45-132.6 45-8.7 0-17.1-.5-25.4-1.5C84.7 611.6 140.8 629 201 629c196.4 0 303.7-160.1 303.7-299 0-4.6-.1-9.1-.3-13.6 20.8-14.9 38.9-33.3 53.2-54.4-19.2 8.3-39.7 14-61.3 16.5 22.2-12.9 39.2-33.4 47.1-58"/></symbol></svg>
 			<!-- endinject -->
 		</div>
 
 		<!-- Scripts -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-			<script>window.jQuery || document.write('<script src="_js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
 	</body>
 </html>
